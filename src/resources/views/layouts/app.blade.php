1962a59e55f37bb056032a410cac3b375a8d0c56
yusukekagawa0417 <y_kagawa0417@yahoo.co.jp>
2021-06-15 13:10:21 +0900

    update: デザインの大枠を作成

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -2,6 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     @include('shared.head')
     <body>
+        @include('shared.header')
         @yield('content')
         @include('shared.footer')
         <script src="{{ asset('js/app.js') }}"></script>
---
+++ src/resources/views/shared/header.blade.php
@@ -0,0 +1,15 @@
+<header class="header">
+    <h1 class="header__title">
+        <a href="/">{{ config('app.name') }}</a>    
+    </h1>
+    <nav class="header__nav">
+        <ul class="header__list">
+            <li class="header__list">
+                <a href="#" class="header__link">link</a>
+            </li>
+            <li class="header__list">
+                <a href="#" class="header__link">link</a>
+            </li>
+        </ul>
+    </nav>
+</header>
