3937fe495840f3465694e4547269e2ad703c8e27
yusukekagawa0417 <y_kagawa0417@yahoo.co.jp>
2021-06-22 17:54:57 +0900

    refactor: インデント調整

--- src/resources/views/product/index.blade.php
+++ src/resources/views/product/index.blade.php
@@ -9,40 +9,40 @@
         <div class="wrapper">
             <div class="card__list">
                 @foreach ($products as $key => $value)
-                <div class="card__item">
-                    <div class="card__contents">
-                        <a target="_blank"
-                        href="{{ $value['url'] }}">
-                            <img class="card__image"
-                                src="{{ $value['image'] }}">
-                        <div class="card__info">
-                            @if ($key + 1 <= 3)
-                            <div class="card__rank card__rank--{{ $key + 1 }}">
-                                <i class="fas fa-crown"></i>&nbsp;{{ $key + 1 }}
-                            </div>
-                            @else
-                            <div class="card__rank">
-                                {{ $key + 1 }}
-                            </div>
-                            @endif
-                            <div class="card__title">
-                                {{ $value['name'] }}
-                            </div>
-                            <div class="card__description">
-                                {{ $value['description'] }}
-                            </div>
-                            <div class="card__categories">
-                                <a class="card__category" href="#">
-                                    本
-                                </a>
-                                <a class="card__category" href="#">
-                                    自己啓発
-                                </a>
-                            </div>
+                    <div class="card__item">
+                        <div class="card__contents">
+                            <a target="_blank"
+                            href="{{ $value['url'] }}">
+                                <img class="card__image"
+                                    src="{{ $value['image'] }}">
+                                <div class="card__info">
+                                    @if ($key + 1 <= 3)
+                                        <div class="card__rank card__rank--{{ $key + 1 }}">
+                                            <i class="fas fa-crown"></i>&nbsp;{{ $key + 1 }}
+                                        </div>
+                                    @else
+                                        <div class="card__rank">
+                                            {{ $key + 1 }}
+                                        </div>
+                                    @endif
+                                    <div class="card__title">
+                                        {{ $value['name'] }}
+                                    </div>
+                                    <div class="card__description">
+                                        {{ $value['description'] }}
+                                    </div>
+                                    <div class="card__categories">
+                                        <a class="card__category" href="#">
+                                            本
+                                        </a>
+                                        <a class="card__category" href="#">
+                                            自己啓発
+                                        </a>
+                                    </div>
+                                </div>
+                            </a>
                         </div>
-                        </a>
                     </div>
-                </div>
                 @endforeach 
             </div>
         </div>
