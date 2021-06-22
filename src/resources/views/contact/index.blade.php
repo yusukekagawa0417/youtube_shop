6aa0125f31dc93801b415328fc7ad637804d6bcb
yusukekagawa0417 <y_kagawa0417@yahoo.co.jp>
2021-06-22 14:27:37 +0900

    update: お問い合わせとプライバシーポリシーの内容を作成

--- src/resources/views/contact/index.blade.php
+++ src/resources/views/contact/index.blade.php
@@ -3,7 +3,8 @@
 @section('content')
     <section>
         <div class="wrapper">
-            <h2>お問い合わせ</h2>
+            <h2 class="title">お問い合わせ</h2>
+            <p class="description">『{{ config('app.name') }}』に関するお問い合わせは、ツイッターアカウント@yusuke_kagawaへのDMにてお願いいたします。</p>
         </div>
     </section>
 @endsection
