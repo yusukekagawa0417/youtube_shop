dcfa6b14b6e438bfc04889083e6657b77d32ecf0
yusukekagawa0417 <y_kagawa0417@yahoo.co.jp>
2021-06-28 16:48:29 +0900

    update: シェアボタンの内容を正式なものに修正

--- src/resources/views/shared/head.blade.php
+++ src/resources/views/shared/head.blade.php
@@ -2,6 +2,11 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="{{ __('messages.app_description') }}">
+    <meta property="og:title" content="{{ config('app.name') }}" />
+    <meta property="og:image" content="{{ asset('image/hero-book2.jpg') }}">
+    <meta property="og:site_name" content="{{ config('app.name') }}">
+    <meta property="og:description" content="{{ __('messages.app_description') }}">
+    <meta name="twitter:card" content="summary_large_image">
     <title>@yield('title', config('app.name'))</title>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" rel="stylesheet">
