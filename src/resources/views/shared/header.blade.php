73a7752835dced5b218cdea785bba756dd9895bd
yusukekagawa0417 <y_kagawa0417@yahoo.co.jp>
2021-06-28 17:25:12 +0900

    fix: 別タブへ遷移させる際の脆弱性対応

--- src/resources/views/shared/header.blade.php
+++ src/resources/views/shared/header.blade.php
@@ -7,18 +7,18 @@
             <li class="header__list">
                 <a class="header__link"
                    href="https://twitter.com/share?
-                    url=https://youtube-rank.net&
-                    hashtags=YouTubeRank&
-                    text={{ __('messages.app_description') }}" 
-                    rel="nofollow" 
-                    target="_blank">
+                   url=https://youtube-rank.net&
+                   hashtags=YouTubeRank&
+                   text={{ __('messages.app_description') }}" 
+                   rel="nofollow noopener noreferrer"
+                   target="_blank">
                     <i class="fab fa-twitter"></i>
                 </a>
             </li>
             <li class="header__list">
                 <a href="#" class="header__link">
                 <a href="http://www.facebook.com/share.php?u=https://youtube-rank.net"
-                   rel="nofollow"
+                   rel="nofollow noopener noreferrer"
                    target="_blank">
                     <i class="fab fa-facebook-f"></i>
                 </a>
