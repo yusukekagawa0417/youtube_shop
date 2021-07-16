ceb86401328104238b2cf92b747eca8d88cd0f1f
yusukekagawa0417 <y_kagawa0417@yahoo.co.jp>
2021-07-16 15:23:08 +0900

    update: 見た目を微修正

--- src/resources/views/shared/hero.blade.php
+++ src/resources/views/shared/hero.blade.php
@@ -1,7 +1,13 @@
 <div class="hero">
     <img src="{{ asset('/image/hero-book2.jpg') }}" class="hero__image">
     <p class="hero__info">
-      YouTubeの動画データを集計し<br>
-      お薦めされている本をランキング化<br>
+      YouTubeの動画データを集計し
+      <br>
+      お薦めされている本をランキング化
+      <br>
+      <br>
+      高評価の商品が一目でわかります
+      <br>
+      ※ <?php echo \Carbon\Carbon::today()->format('Y/m/d') ?> 更新
     </p>
 </div>
