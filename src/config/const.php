06307705523c0962c0009e86f0d16e2e3202bcac
yusukekagawa0417 <y_kagawa0417@yahoo.co.jp>
2021-07-17 18:10:37 +0900

    add: コンテンツ追加

--- src/config/const.php
+++ src/config/const.php
@@ -4,5 +4,7 @@ return [
     'youtube_api_key' => env('YOUTUBE_API_KEY', 'xxxxx'),
     'channel_ids' => [
         '学識サロン' => 'UCC4NkFV-L-vVYD5z_Ei5dUA',
+        'サラタメさん【サラリーマンYouTuber】' => 'UCaG7jufgiw4p5mphPPVbqhw',
+        '文学YouTuberベル' => 'UCL4QAojeGy6CJ9R2PwmlmJQ',
     ]
 ];
